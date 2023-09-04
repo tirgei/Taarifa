bb09c666ba99eb4d5591aad2da928f88a0ad7dac
Vincent Tirgei <tirgeic@gmail.com>
2023-09-04 16:16:42 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Taarifa"
+include(":androidApp")
+include(":shared")
